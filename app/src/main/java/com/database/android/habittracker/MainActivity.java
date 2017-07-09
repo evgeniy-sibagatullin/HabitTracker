38a6415de3d3fac2b3c29a4daa51f948916cb63d
evgeniy-sibagatullin <evgeniy.sibagatullin@gmail.com>
2017-07-09 22:34:54 +0300

    Provide HabitTrackerDbHelper class initial version.

--- app/src/main/java/com/database/android/habittracker/MainActivity.java
+++ app/src/main/java/com/database/android/habittracker/MainActivity.java
@@ -1,7 +1,13 @@
 package com.database.android.habittracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.database.android.habittracker.data.HabitContract.HabitEntry;
+import com.database.android.habittracker.data.HabitTrackerDbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        displayDatabaseInfo();
+    }
+
+    private void displayDatabaseInfo() {
+        HabitTrackerDbHelper dbHelper = new HabitTrackerDbHelper(this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + HabitEntry.TABLE_NAME, null);
+        TextView dbData = (TextView) findViewById(R.id.db_data);
+
+        try {
+            dbData.setText(String.format(getString(R.string.rows_in_table), cursor.getCount()));
+        } finally {
+            cursor.close();
+        }
     }
 }
---
+++ app/src/main/java/com/database/android/habittracker/data/HabitTrackerDbHelper.java
@@ -0,0 +1,34 @@
+package com.database.android.habittracker.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.database.android.habittracker.data.HabitContract.HabitEntry;
+
+public class HabitTrackerDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "habit_tracker.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public HabitTrackerDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_HABITS_TABLE = "CREATE TABLE " + HabitEntry.TABLE_NAME + " ("
+                + HabitEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + HabitEntry.COLUMN_HABIT_TITLE + " TEXT NOT NULL, "
+                + HabitEntry.COLUMN_HABIT_AGE_WEEKS + " INTEGER NOT NULL, "
+                + HabitEntry.COLUMN_HABIT_USEFULNESS + " INTEGER NOT NULL DEFAULT 2, "
+                + HabitEntry.COLUMN_HABIT_NOTE + " TEXT);";
+        sqLiteDatabase.execSQL(SQL_CREATE_HABITS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
